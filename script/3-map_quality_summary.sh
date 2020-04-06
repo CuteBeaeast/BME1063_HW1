2843b38af1144ef0acc3466348ea4eab3ec061ac
CuteBeaeast <827019851@qq.com>
2020-04-06 22:04:04 +0800

    modify map quality summary script

--- script/3-map_quality_summary.sh
+++ script/3-map_quality_summary.sh
@@ -6,14 +6,12 @@ for P in ${BWA}/dataset*
 do
 cd ${P}
 FILE=./*.sam
-echo "thr	mc	mi	mu	um	uu	mc+mi+mu+um+uu	mc'	mi'	mu'	um'	uu'	mc'+mi'+mu'+um'+uu'	mc/(mc'+mi'+mu')	mc/(mc'+mi')	um/(um'+uu')	mc'/(mc'+mi'_mu')	mc'/(mc'+mi')	um'/(um'+uu')" > ${OPATH}/bwa_mapping/$(basename ${FILE}).quality
-dwgsim_eval -S ${FILE} 2>&- | sed '/#/d' >> ${OPATH}/bwa_mapping/$(basename ${FILE}).quality
+dwgsim_eval -S ${FILE} 2>&- | sed '/#/d' > ${OPATH}/bwa_mapping/$(basename ${FILE}).quality
 done
 
 for P in ${BOWTIE}/dataset*
 do
 cd ${P}
 FILE=./*.sam
-echo "thr	mc	mi	mu	um	uu	mc+mi+mu+um+uu	mc'	mi'	mu'	um'	uu'	mc'+mi'+mu'+um'+uu'	mc/(mc'+mi'+mu')	mc/(mc'+mi')	um/(um'+uu')	mc'/(mc'+mi'_mu')	mc'/(mc'+mi')	um'/(um'+uu')" > ${OPATH}/bowtie2_mapping/$(basename ${FILE}).quality
-dwgsim_eval -S ${FILE} 2>&- | sed '/#/d' >> ${OPATH}/bowtie2_mapping/$(basename ${FILE}).quality
+dwgsim_eval -S ${FILE} 2>&- | sed '/#/d' > ${OPATH}/bowtie2_mapping/$(basename ${FILE}).quality
 done
